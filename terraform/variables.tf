5a6676d9868f7d2ae96dd53828587fdc59769591
Joshua <josh.timmons@mongodb.com>
2024-10-27 23:00:44 -0400

    Remove some vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,3 @@
-variable "github_idp_client_id" {
-  type = string
-}
-
-variable "github_idp_client_secret" {
-  type      = string
-  sensitive = true
-}
-
 variable "minio_admin_password" {
   type      = string
   sensitive = true
@@ -20,22 +11,4 @@ variable "minio_access_key" {
 variable "minio_secret_key" {
   type      = string
   sensitive = true
-}
-
-variable "nr_api_key" {
-  type      = string
-  sensitive = true
-}
-
-variable "openvpn_user" {
-  type = string
-}
-
-variable "openvpn_password" {
-  type      = string
-  sensitive = true
-}
-
-variable "streaming_emails" {
-  type = list(string)
 }
